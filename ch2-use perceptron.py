f377679123ffb64f83c0bafe8b0ea495f42623ee
zhen.li <zhen.li@acacceptance.com>
2019-06-07 13:51:07 -0400

    update

---
+++ ch2-use perceptron.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 09:43:21 2019
+
+@author: Zhen.Li
+"""
+
+# use Perceptron script
+from PerceptronCh2 import Perceptron
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+df.tail()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color = 'red', marker = 'o', label = 'setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color = 'blue', marker = 'x', label = 'versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc = 'upper left')
+plt.show()
+
+ppn = Perceptron(eta = 0.1, n_iter = 10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1, ppn.errors_, marker = 'o') )
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
+plt.show()
+
