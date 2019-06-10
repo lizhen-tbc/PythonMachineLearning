8288d86b8013bceb2fd42be6d4149bd22d529051
zhen.li <zhen.li@acacceptance.com>
2019-06-10 16:46:24 -0400

    add new chapter 2  page 32

---
+++ ch2-draw decision boundary.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 08:46:32 2019
+
+@author: Zhen.Li
+"""
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arrange(x1_min, x1_max, resolution),
+                           np.arrange(x2_min, x2_max, resolution))
+    z = classifier.
+    
