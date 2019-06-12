749990de9baba1d37ca4c9e0691136e173b4e51c
zhen.li <zhen.li@acacceptance.com>
2019-06-12 08:27:28 -0400

    pg 40, 43

--- ch2-use Adalin.py
+++ ch2-use Adalin.py
@@ -35,3 +35,26 @@ ax[2].set_ylabel('log(Sum-squared-error)')
 ax[2].set_title('Adaline - Learning rate 0.001')
 
 plt.show()
+
+#%%
+X_std = np.copy(X)
+X_std[:,0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:,1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter = 15, eta = 0.01)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier = ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc = 'upper left')
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
+
+ 
