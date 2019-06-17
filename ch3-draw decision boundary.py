8d047c7eaf5150874b7dd1d5e2a829b598afdaf0
zhen.li <zhen.li@acacceptance.com>
2019-06-17 09:02:49 -0400

    p56-57

--- ch3-draw decision boundary.py
+++ ch3-draw decision boundary.py
@@ -28,6 +28,12 @@ def plot_decision_regions(X, y, classifier, test_idx = None, resolution = 0.02):
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
     
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1],
+                    alpha = 0.8, c = colors[idx],
+                    marker = markers[idx], label = cl, 
+                    edgecolor = 'black')
+    
     #highlight test samples
     if test_idx:
         #plot all samples
--- ch3-sklearn.py
+++ ch3-sklearn.py
@@ -71,9 +71,17 @@ plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc = 'upper left')
 plt.show()
-#%% p56
- 
-
+#%% p57
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X = X_combined_std,
+                      y = y_combined,
+                      classifier = ppn, 
+                      test_idx = range(105, 150))
+plt.xlabel('petal length ')
+plt.ylabel('petal width ')
+plt.legend(loc = 'upper left')
+plt.show()
 
 
 
